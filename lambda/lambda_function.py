d31dbdb395d48d08c0c9f7d6959eb72ddbe8588e
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-09-18 21:39:12 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -58,7 +58,7 @@ class CaptureUsernameIDIntentHandler(AbstractRequestHandler):
 
         response = requests.get(
             'https://api.twitter.com/2/tweets',
-            headers={'authorization': 'Bearer {}'.format('AAAAAAAAAAAAAAAAAAAAAHW1HgEAAAAAAH%2BIRqvHUQiRtNB%2BI6pYSFkiEhQ%3DQpkPWVQzI7mTi2WYkMPDsuXF6LcZ5yTqCUicawbDqx6UXdzi5u')},
+            headers={'authorization': 'Bearer' + 'AAAAAAAAAAAAAAAAAAAAAHW1HgEAAAAAAH%2BIRqvHUQiRtNB%2BI6pYSFkiEhQ%3DQpkPWVQzI7mTi2WYkMPDsuXF6LcZ5yTqCUicawbDqx6UXdzi5u')},
             params=parameters)
         
         
