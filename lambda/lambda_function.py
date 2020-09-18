d3c92342e7359ddb0195025383bb30f9b5704307
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-09-18 23:30:02 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -100,7 +100,7 @@ class GetCovidNumbersIntentHandler(AbstractRequestHandler):
         rc = response.status_code
         res = response.json()
             
-        speak_output = "The topic you said is {topic} and the response code is {rc} and the response is {idens}".format(topic=topic, rc=rc, idens=idens)
+        speak_output = "The response code is {rc} and the response is {idens}".format(rc=rc, res=res)
 
         return (
             handler_input.response_builder
