218ee12a57140ad25e156013502426c69abc2198
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-09-19 21:20:38 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -110,7 +110,8 @@ class GetCovidNumbersIntentHandler(AbstractRequestHandler):
         for index, state in df.iterrows():
         
             if state[0] == state_slot:
-                print('The state of', states[state[0]], 'has had', state[3], 'deaths')
+                # print('The state of', states[state[0]], 'has had', state[3], 'deaths')
+                speak_output = 'The state of {} has had {} deaths'.format(states[state[0]], state[3])
 
         speak_output = "The response code is {rc} and the response is {res}".format(rc=rc, res=res)
 
