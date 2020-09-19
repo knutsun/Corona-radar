e7ab0a95ac19d569a4ecf9e9569e09a3c93a1d3f
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-09-19 21:35:22 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -15,7 +15,7 @@ from ask_sdk_core.handler_input import HandlerInput
 from ask_sdk_model import Response
 
 import csv
-import pandas
+import pandas as pd
 import requests
 
 logger = logging.getLogger(__name__)
