0b54193fe2f18c794b357146a20859de9e927692
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-09-20 23:46:37 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -94,19 +94,59 @@ class GetCovidNumbersIntentHandler(AbstractRequestHandler):
         df = pd.read_csv(STATES_URL)
         
         states = {
-            "AL": "Alabama",
-            "AK": "Alaska"
+                    "AL": "Alabama",
+                    "AK": "Alaska",
+                    "AR": "Arkansas",
+                    "CA": "California",
+                    "CO": "Colorado",
+                    "CT": "Connecticut",
+                    "DC": "Washington D.C.",
+                    "DE": "Delaware",
+                    "FL": "Florida",
+                    "GA": "Georgia",
+                    "HI": "Hawaii",
+                    "ID": "Idaho",
+                    "IL": "Illinois",
+                    "IN": "Indiana",
+                    "IA": "Iowa",
+                    "KS": "Kansas",
+                    "KY": "Kentucky",
+                    "LA": "Louisiana",
+                    "ME": "Maine",
+                    "MD": "Maryland",
+                    "MA": "Massachusetts",
+                    "MI": "Michigan",
+                    "MN": "Minnesota",
+                    "MS": "Mississippi",
+                    "MO": "Missouri",
+                    "MT": "Montana",
+                    "NE": "Nebraska",
+                    "NV": "Nevada",
+                    "NH": "New Hampshire",
+                    "NJ": "New Jersey",
+                    "NM": "New Mexico",
+                    "NY": "New York",
+                    "NC": "North Carolina",
+                    "ND": "North Dakota",
+                    "OH": "Ohio",
+                    "OK": "Oklahoma",
+                    "OR": "Oregon",
+                    "PA": "Pennsylvania",
+                    "PR": "Puerto Rico",
+                    "RI": "Rhode Island",
+                    "SC": "South Carolina",
+                    "SD": "South Dakota",
+                    "TN": "Tennessee",
+                    "TX": "Texas",
+                    "UT": "Utah",
+                    "VT": "Vermont",
+                    "VA": "Virginia",
+                    "WA": "Washington",
+                    "WV": "West Virginia",
+                    "WI": "Wisconsin",
+                    "WY": "Wyoming"
+                }
 
-
-
-            # "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA",
-            # "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD",
-            # "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ",
-            # "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "PR", "RI", "SC",
-            # "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"
-
-        }
-        
         speak_output = ''
 
 
