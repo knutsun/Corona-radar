d93b3ba113fd1e96ff0d1a8d450c7513db4234f3
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-09-22 03:13:43 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -207,7 +207,6 @@ class GetTopCovidNumbersIntentHandler(AbstractRequestHandler):
 
         ordered_struc = sorted(data_struc.items(), key=lambda x: x[1], reverse=True)
 
-        # speak_output = speak_output + str(ordered_struc[:number_slot])
         for state, count in enumerate(ordered_struc[:number_slot]):
             speak_output = speak_output + str(count[0]) + str(count[1])
 
