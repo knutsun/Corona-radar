2b39685ccf4dd99cdda66b1a57d97d2702b3877e
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-09-23 23:29:29 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -19,6 +19,59 @@ import UrlsEnum
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+states = {
+                    "AL": "Alabama",
+                    "AK": "Alaska",
+                    "AR": "Arkansas",
+                    "CA": "California",
+                    "CO": "Colorado",
+                    "CT": "Connecticut",
+                    "DC": "Washington D.C.",
+                    "DE": "Delaware",
+                    "FL": "Florida",
+                    "GA": "Georgia",
+                    "HI": "Hawaii",
+                    "ID": "Idaho",
+                    "IL": "Illinois",
+                    "IN": "Indiana",
+                    "IA": "Iowa",
+                    "KS": "Kansas",
+                    "KY": "Kentucky",
+                    "LA": "Louisiana",
+                    "ME": "Maine",
+                    "MD": "Maryland",
+                    "MA": "Massachusetts",
+                    "MI": "Michigan",
+                    "MN": "Minnesota",
+                    "MS": "Mississippi",
+                    "MO": "Missouri",
+                    "MT": "Montana",
+                    "NE": "Nebraska",
+                    "NV": "Nevada",
+                    "NH": "New Hampshire",
+                    "NJ": "New Jersey",
+                    "NM": "New Mexico",
+                    "NY": "New York",
+                    "NC": "North Carolina",
+                    "ND": "North Dakota",
+                    "OH": "Ohio",
+                    "OK": "Oklahoma",
+                    "OR": "Oregon",
+                    "PA": "Pennsylvania",
+                    "PR": "Puerto Rico",
+                    "RI": "Rhode Island",
+                    "SC": "South Carolina",
+                    "SD": "South Dakota",
+                    "TN": "Tennessee",
+                    "TX": "Texas",
+                    "UT": "Utah",
+                    "VT": "Vermont",
+                    "VA": "Virginia",
+                    "WA": "Washington",
+                    "WV": "West Virginia",
+                    "WI": "Wisconsin",
+                    "WY": "Wyoming"
+                }
 
 class LaunchRequestHandler(AbstractRequestHandler):
     """Handler for Skill Launch."""
@@ -51,65 +104,10 @@ class GetCovidNumbersIntentHandler(AbstractRequestHandler):
         
         slots = handler_input.request_envelope.request.intent.slots
         state_slot = slots["state"].value
-        
+
         df = pd.read_csv('http://coronavirusapi.com/states.csv')
         
-        states = {
-                    "AL": "Alabama",
-                    "AK": "Alaska",
-                    "AR": "Arkansas",
-                    "CA": "California",
-                    "CO": "Colorado",
-                    "CT": "Connecticut",
-                    "DC": "Washington D.C.",
-                    "DE": "Delaware",
-                    "FL": "Florida",
-                    "GA": "Georgia",
-                    "HI": "Hawaii",
-                    "ID": "Idaho",
-                    "IL": "Illinois",
-                    "IN": "Indiana",
-                    "IA": "Iowa",
-                    "KS": "Kansas",
-                    "KY": "Kentucky",
-                    "LA": "Louisiana",
-                    "ME": "Maine",
-                    "MD": "Maryland",
-                    "MA": "Massachusetts",
-                    "MI": "Michigan",
-                    "MN": "Minnesota",
-                    "MS": "Mississippi",
-                    "MO": "Missouri",
-                    "MT": "Montana",
-                    "NE": "Nebraska",
-                    "NV": "Nevada",
-                    "NH": "New Hampshire",
-                    "NJ": "New Jersey",
-                    "NM": "New Mexico",
-                    "NY": "New York",
-                    "NC": "North Carolina",
-                    "ND": "North Dakota",
-                    "OH": "Ohio",
-                    "OK": "Oklahoma",
-                    "OR": "Oregon",
-                    "PA": "Pennsylvania",
-                    "PR": "Puerto Rico",
-                    "RI": "Rhode Island",
-                    "SC": "South Carolina",
-                    "SD": "South Dakota",
-                    "TN": "Tennessee",
-                    "TX": "Texas",
-                    "UT": "Utah",
-                    "VT": "Vermont",
-                    "VA": "Virginia",
-                    "WA": "Washington",
-                    "WV": "West Virginia",
-                    "WI": "Wisconsin",
-                    "WY": "Wyoming"
-                }
-
         speak_output = ''
-
 
         for index, state in df.iterrows():
             for index, st in enumerate(states):
@@ -138,60 +136,6 @@ class GetTopCovidNumbersIntentHandler(AbstractRequestHandler):
 
         df = pd.read_csv('http://coronavirusapi.com/states.csv')
         
-        states = {
-                    "AL": "Alabama",
-                    "AK": "Alaska",
-                    "AR": "Arkansas",
-                    "CA": "California",
-                    "CO": "Colorado",
-                    "CT": "Connecticut",
-                    "DC": "Washington D.C.",
-                    "DE": "Delaware",
-                    "FL": "Florida",
-                    "GA": "Georgia",
-                    "HI": "Hawaii",
-                    "ID": "Idaho",
-                    "IL": "Illinois",
-                    "IN": "Indiana",
-                    "IA": "Iowa",
-                    "KS": "Kansas",
-                    "KY": "Kentucky",
-                    "LA": "Louisiana",
-                    "ME": "Maine",
-                    "MD": "Maryland",
-                    "MA": "Massachusetts",
-                    "MI": "Michigan",
-                    "MN": "Minnesota",
-                    "MS": "Mississippi",
-                    "MO": "Missouri",
-                    "MT": "Montana",
-                    "NE": "Nebraska",
-                    "NV": "Nevada",
-                    "NH": "New Hampshire",
-                    "NJ": "New Jersey",
-                    "NM": "New Mexico",
-                    "NY": "New York",
-                    "NC": "North Carolina",
-                    "ND": "North Dakota",
-                    "OH": "Ohio",
-                    "OK": "Oklahoma",
-                    "OR": "Oregon",
-                    "PA": "Pennsylvania",
-                    "PR": "Puerto Rico",
-                    "RI": "Rhode Island",
-                    "SC": "South Carolina",
-                    "SD": "South Dakota",
-                    "TN": "Tennessee",
-                    "TX": "Texas",
-                    "UT": "Utah",
-                    "VT": "Vermont",
-                    "VA": "Virginia",
-                    "WA": "Washington",
-                    "WV": "West Virginia",
-                    "WI": "Wisconsin",
-                    "WY": "Wyoming"
-                }
-
         speak_output = ''
 
         data_struc = {
