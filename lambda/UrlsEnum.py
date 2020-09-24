9ae560f4a80b0d980af804f3db42689b3581ee09
VoiceHubCodeCommitRole/VoiceHubCodeCommitRole <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-09-24 00:04:13 +0000

    Deleted UrlsEnum.py

--- lambda/UrlsEnum.py
+++
@@ -1,8 +0,0 @@
-import enum
-
-
-class UrlsEnum(enum.Enum):
-
-    States = 'http://coronavirusapi.com/states.csv'
-    Time_series = 'http://coronavirusapi.com/time_series.csv'
-    Time_series_by_state = 'http://coronavirusapi.com/getTimeSeries/'
