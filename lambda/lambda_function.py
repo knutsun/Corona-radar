86f462cf5b06ce036a99af2f3f433160a5763a12
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-09-26 22:29:32 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -116,7 +116,7 @@ class GetLocationIntentHandler(AbstractRequestHandler):
         #         'Accept: application/json'  })
         
         
-        speak_output = '{}, {}'.format(deviceId, accessToken)
+        speak_output = '{}, {}'.format(deviceId)
 
         return (
             handler_input.response_builder
