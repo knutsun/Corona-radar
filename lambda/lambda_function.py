e4d677b1639be13ea53156f0574c6f4a3ac12f90
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-09-26 22:08:03 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -329,10 +329,10 @@ class CatchAllExceptionHandler(AbstractExceptionHandler):
 sb = SkillBuilder()
 
 sb.add_request_handler(LaunchRequestHandler())
+sb.add_request_handler(GetLocationIntentHandler())
 sb.add_request_handler(GetCovidNumbersIntentHandler())
 sb.add_request_handler(GetTopCovidNumbersIntentHandler())
 sb.add_request_handler(GetBottomCovidNumbersIntentHandler())
-sb.add_request_handler(GetLocationIntentHandler())
 sb.add_request_handler(HelpIntentHandler())
 sb.add_request_handler(CancelOrStopIntentHandler())
 sb.add_request_handler(SessionEndedRequestHandler())
