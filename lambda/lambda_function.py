803cd46af460be11d44b0bedc1181aa785809fa6
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-09-27 22:11:11 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -111,7 +111,7 @@ class GetLocationIntentHandler(AbstractRequestHandler):
         # state_slot = slots["state"].value
         # deviceId = this.event.context.System.device.deviceId
         # accessToken = this.event.context.System.apiAccessToken
-        d = handler_input.request_envelope.context.system.device.deviceId
+        d = handler_input.request_envelope.context.system.device.device_id
         # response = requests.get('https://api.amazonalexa.com/v1/devices/{}/settings/address'.format(str(deviceId)),
         # headers={'Authorization: Bearer '.format(str(accessToken)),
         #         'Accept: application/json'  })
